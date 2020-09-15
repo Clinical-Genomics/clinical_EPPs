034830110816dca53e795c629da7cf367d85043a
Clarity LIMS - Automated Informatics User <glsai@clinical-lims-stage.scilifelab.se>
2020-09-15 13:13:10 +0200

    click

--- EPPs/move_samples.py
+++ EPPs/move_samples.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from argparse import ArgumentParser
-from clinical_EPPs.exceptions import QueueArtifactsError
+
+from clinical_EPPs.exceptions import Clinical_EPPsError, QueueArtifactsError
 
 from genologics.lims import Lims
 from genologics.config import BASEURI,USERNAME,PASSWORD
 from genologics.entities import Process
+
 import sys
+import click
+
 
 DESC = """Script for queuing artifacts."""
-
 
 
 def get_artifacts(process, input_analyte):
@@ -22,43 +23,36 @@ def get_artifacts(process, input_analyte):
     return artifacts
 
 
-def queue_artifacts(artifacts, workflow_id, stage_id):
+def queue_artifacts(lims, artifacts, workflow_id, stage_id):
     """"""
 
-    stage_uri = f'{BASEURI}/api/v2/configuration/workflows/{workflow_id}/stages/{stage_id}'
-
+    #stage_uri = f'{BASEURI}/api/v2/configuration/workflows/{workflow_id}/stages/{stage_id}'
+    stage_uri = BASEURI+'/api/v2/configuration/workflows/'+workflow_id+'/stages/' + stage_id
     try:
         lims.route_artifacts(artifacts, stage_uri=stage_uri)
     except:
         raise QueueArtifactsError('Failed to queue artifacts.')
          
-  
-def main(lims, args):
-    process = Process(lims, id = args.process)
-    artifacts = get_artifacts(process, args.input_analyte)
+option_process = click.option('-p', '--process', required=True, help='Lims id for current Process')
+
+@click.command()
+@option_process
+@click.option('-w', '--workflow', required=True, help='Destination workflow id.')
+@click.option('-s', '--stage', required=True, help='Destination stage id.')
+@click.option('-u', '--udf', required=True, help='UDF that will tell wich artifacts to move.')
+@click.option('-i', '--input-analyte', default=False, is_flag=True,
+                       help='Use this flag if you run the script from a QC step.')
+def main(process, workflow, stage, udf, input_analyte):
+    lims = Lims(BASEURI, USERNAME, PASSWORD)
+    process = Process(lims, id = process)
+    artifacts = get_artifacts(process, input_analyte)
 
     try:
-        queue_artifacts(artifacts, args.workflow, args.stage)
-    except NIPToolError as e:
+        queue_artifacts(lims, artifacts, workflow, stage)
+    except Clinical_EPPsError as e:
         sys.exit(e.message)
-        #raise click.Abort()
 
 if __name__ == "__main__":
-    ## change to click
-    parser = ArgumentParser(description=DESC)
-    parser.add_argument('-p', dest='process', 
-                        help='Lims id for current Process')
-    parser.add_argument('-w', dest='workflow',
-                        help='Destination workflow id.')
-    parser.add_argument('-s', dest='stage',
-                        help='Destination stage id.')
-    parser.add_argument('-u', dest='udf',
-                        help='UDF that will tell wich artifacts to move.')
-    parser.add_argument('-i', dest='input_analyte', action='store_true',
-                        help='Use this tag if you run the script from a QC step.')
-    
-    args = parser.parse_args()
-    lims = Lims(BASEURI, USERNAME, PASSWORD)
-    main(lims, args)
+    main()
 
 
